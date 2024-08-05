fff759ac28d56eb3180fcef9a8bd3c6d8b49e054
eb9862 <luclead622@gmail.com>
2024-08-05 14:31:50 +0900

    Fixed : minishell.h

--- heredoc_handler_utils.c
+++ heredoc_handler_utils.c
@@ -6,9 +6,11 @@
 /*   By: eunhwang <eunhwang@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 00:09:01 by eunhwang          #+#    #+#             */
-/*   Updated: 2024/08/05 00:09:20 by eunhwang         ###   ########.fr       */
+/*   Updated: 2024/08/05 14:25:48 by eunhwang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "minishell.h"
 
 void	check_line(char **line, int *ctrl_d)
 {
